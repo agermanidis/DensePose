ff2813a844caa23604724d41e9257fea5a90ce06
Anastasis Germanidis <agermanidis@gmail.com>
2019-04-18 16:51:24 -0400

    add install script

---
+++ install.sh
@@ -0,0 +1,34 @@
+apt-get update
+apt-get install -y --no-install-recommends \
+      build-essential \
+      git \
+      libgoogle-glog-dev \
+      libgtest-dev \
+      libiomp-dev \
+      libleveldb-dev \
+      liblmdb-dev \
+      libopencv-dev \
+      libopenmpi-dev \
+      libsnappy-dev \
+      libprotobuf-dev \
+      openmpi-bin \
+      openmpi-doc \
+      protobuf-compiler \
+      python-dev \
+      libgflags-dev \
+      cmake
+pip install --user \
+      future \
+      numpy \
+      protobuf \
+      typing \
+      Cython \
+      hypothesis
+git clone https://github.com/pytorch/pytorch.git && cd pytorch
+git submodule update --init --recursive
+python2 setup.py install
+python2 -c 'from caffe2.python import core'
+pip install -U Cython
+pip install -r requirements.txt
+make
+make ops
